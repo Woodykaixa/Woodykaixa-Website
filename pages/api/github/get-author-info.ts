1632e90bfca9f94dbd50221114319165218c9852
kaixa <690750353@qq.com>
2022-01-01 01:03:39 +0800

    feat(api): get author info

---
+++ pages/api/github/get-author-info.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { GitHubGetUsersDTO } from '../../../dto';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<
+    | GitHubGetUsersDTO
+    | {
+        error: string;
+        desc: string;
+      }
+  >
+) {
+  fetch('https://api.github.com/users/Woodykaixa', {
+    headers: {
+      accept: 'application/vnd.github.v3+json',
+    },
+  })
+    .then(r => r.json() as Promise<GitHubGetUsersDTO>)
+    .then(json => {
+      res.status(200).json(json);
+    })
+    .catch((err: Error) => {
+      res.status(400).json({
+        error: err.name,
+        desc: err.message,
+      });
+    });
+}
