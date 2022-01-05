16203a31eb730149bfa3b1165fe870a386d6c16e
kaixa <690750353@qq.com>
2022-01-05 19:34:41 +0800

    feat(markdown): Hidden tag transition

--- components/markdown/Hidden.tsx
+++ components/markdown/Hidden.tsx
@@ -1,5 +1,7 @@
 import React, { ReactChildren } from 'react';
 
 export function Hidden(props: { children: ReactChildren }) {
-  return <span className='bg-black text-black hover:text-white'>{props.children}</span>;
+  return (
+    <span className='bg-black text-black hover:text-white ease-linear duration-200 transition'>{props.children}</span>
+  );
 }
