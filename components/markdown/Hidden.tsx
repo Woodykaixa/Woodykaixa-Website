b2d7f47742e10bf3d7f4f0eb4db157d39874cc08
kaixa <690750353@qq.com>
2022-01-05 22:33:36 +0800

    fix(markdown): fix hidden url color

--- components/markdown/Hidden.tsx
+++ components/markdown/Hidden.tsx
@@ -2,6 +2,8 @@ import React, { ReactChildren } from 'react';
 
 export function Hidden(props: { children: ReactChildren }) {
   return (
-    <span className='bg-black text-black hover:text-white ease-linear duration-200 transition'>{props.children}</span>
+    <span className='hidden-wrapper bg-black text-black hover:text-white ease-linear duration-200 transition'>
+      {props.children}
+    </span>
   );
 }
