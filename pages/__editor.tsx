9545846f61bc252d1700f5b1951a8e8b8c5b736a
kaixa <690750353@qq.com>
2022-01-05 19:08:36 +0800

    feat(editor): markdown rendering

--- pages/__editor.tsx
+++ pages/__editor.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
 import { Input, Typography, Button, Form } from 'antd';
+import Markdown from 'markdown-to-jsx';
+import { IndexOptions } from '@/config/markdown';
+import { useState } from 'react';
+
 const Me: NextPage = () => {
   const [form] = Form.useForm<{ name: string; content: string }>();
+  const [t, setT] = useState('');
   const get = () => {
     fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/oss/get?name=' + form.getFieldsValue().name)
       .then(res => res.json())
@@ -9,6 +14,7 @@ const Me: NextPage = () => {
         form.setFieldsValue({
           content: json.content,
         });
+        setT(json.content);
       });
   };
 
@@ -19,6 +25,8 @@ const Me: NextPage = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(form.getFieldsValue()),
+    }).then(() => {
+      setT(form.getFieldsValue().content);
     });
   };
 
@@ -30,15 +38,21 @@ const Me: NextPage = () => {
         autoComplete='on'
         className='items-center p-4 max-w-5xl w-full'
         form={form}
+        initialValues={{ content: '12' }}
       >
         <Form.Item name='name'>
           <Input></Input>
         </Form.Item>
-        <Typography>
-          <Form.Item name='content'>
-            <Input.TextArea rows={30}></Input.TextArea>
-          </Form.Item>
-        </Typography>
+        <div className='flex'>
+          <Typography className='w-1/2'>
+            <Form.Item name='content'>
+              <Input.TextArea rows={30}></Input.TextArea>
+            </Form.Item>
+          </Typography>
+          <div className='w-1/2'>
+            <Markdown options={IndexOptions}>{t}</Markdown>
+          </div>
+        </div>
         <Button onClick={get}>get</Button>
         <Button onClick={put}>post</Button>
       </Form>
