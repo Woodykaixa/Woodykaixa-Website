e3f98138c07f7796a58e631229774bd7e3ed6414
kaixa <690750353@qq.com>
2022-01-08 21:03:40 +0800

    fix(env): fix env def

--- env.d.ts
+++ env.d.ts
@@ -1,18 +1,4 @@
 import type { PrismaClient } from '@prisma/client';
-declare namespace NodeJS {
-  interface ProcessEnv {
-    APP_ENV: 'development' | 'production' | 'staging';
-    PORT?: string;
-    NEXT_PUBLIC_BASE_URL: string;
-    NEXT_PUBLIC_GITHUB_CLIENT_ID: string;
-    GITHUB_CLIENT_SECRET: string;
-    OSS_BUCKET: string;
-    OSS_REGION: string;
-    ACCESS_KEY_ID: string;
-    ACCESS_KEY_SECRET: string;
-    DATABASE_URL: string;
-  }
-}
 
 declare global {
   // PrismaClient is attached to the `global` object in development to prevent
@@ -22,6 +8,21 @@ declare global {
   // https://pris.ly/d/help/next-js-best-practices
   // So we only use this global var in development mode
   var prisma: PrismaClient;
+
+  declare namespace NodeJS {
+    interface ProcessEnv {
+      APP_ENV: 'development' | 'production' | 'staging';
+      PORT?: string;
+      NEXT_PUBLIC_BASE_URL: string;
+      NEXT_PUBLIC_GITHUB_CLIENT_ID: string;
+      GITHUB_CLIENT_SECRET: string;
+      OSS_BUCKET: string;
+      OSS_REGION: string;
+      ACCESS_KEY_ID: string;
+      ACCESS_KEY_SECRET: string;
+      DATABASE_URL: string;
+    }
+  }
 }
 
 export {};
