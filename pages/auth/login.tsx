3bf6edc20f9c29d06e99f6ccc454c625a5f67604
kaixa <690750353@qq.com>
2022-01-12 19:27:51 +0800

    feat(login): update ui

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import { JwtConfig } from '@/config/jwt';
 import { firstValue } from '@/util/api';
-import { message, Spin } from 'antd';
+import { message, Spin, Result } from 'antd';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
@@ -18,8 +18,12 @@ export default function LoginPage() {
     }
   }, [router]);
   return (
-    <div className='w-full'>
-      <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} delay={200} tip={'加载中，请稍候……'}></Spin>
+    <div className='m-16 flex justify-center'>
+      <Result
+        icon={<Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} delay={200}></Spin>}
+        title={'加载中，请稍候……'}
+        className='p-24 bg-white py-44 w-full max-w-7xl'
+      ></Result>
     </div>
   );
 }
