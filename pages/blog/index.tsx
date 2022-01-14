12813bc58e6aabd74b8ba069220aec8f91e1ceb1
kaixa <690750353@qq.com>
2022-01-14 18:46:35 +0800

    feat(blog): search by meta tags

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,12 +1,14 @@
 import type { GetServerSideProps, NextPage } from 'next';
-import { List, Input, Space, Tag } from 'antd';
-import { MessageOutlined } from '@ant-design/icons';
+import { List, Input, Space, Tag, Form, Mentions, Button } from 'antd';
+import { MessageOutlined, SearchOutlined } from '@ant-design/icons';
 import * as React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Err, OK } from '@/dto';
 import { SiteConfig } from '@/config/site';
+import Head from 'next/head';
+import { SearchTags } from '@/util/search';
 
 const { Search } = Input;
 type PostType = {
@@ -45,28 +47,60 @@ const Post = ({ title, createAt, keywords, comments, content, id, description, c
 
 const Blog: NextPage<ServerSideProps> = ({ files }) => {
   return (
-    <div className='bg-white p-6 flex flex-col items-center mt-8'>
-      <div className='flex flex-col w-3/4 items-center'>
-        <List
-          header={<Search enterButton allowClear></Search>}
-          itemLayout='vertical'
-          size='large'
-          pagination={{
-            onChange: page => {
-              console.log(page);
-            },
-            pageSize: 5,
-          }}
-          dataSource={files}
-          footer={
-            <div>
-              还没仔细研究过，不过本站的所有文章应该会使用 <b>CC-BY-SA-4.0</b> 协议
-            </div>
-          }
-          renderItem={item => <Post {...item} />}
-        />
+    <>
+      <Head>
+        <meta name='og:title' content={'博客 | ' + SiteConfig.title} />
+        <title>博客 | {SiteConfig.title}</title>
+        <meta name='og:type' content='website' />
+        {process.env.APP_ENV === 'production' && (
+          <meta name='og:url' content={process.env.NEXT_PUBLIC_BASE_URL + '/blog'} />
+        )}
+        <meta name='og:locale' content='zh_CN' />
+        <meta property='og:description' content="Woodykaixa's personal website. Blog, personal net dist, etc." />
+      </Head>
+      <div className='bg-white p-6 flex flex-col items-center mt-8'>
+        <div className='flex flex-col w-3/4 items-center'>
+          <List
+            header={
+              <Form
+                onFinish={e => {
+                  console.log(e);
+                }}
+              >
+                <Form.Item name='search' label='搜索'>
+                  <div className='flex'>
+                    <Mentions
+                      autoSize={{ maxRows: 1, minRows: 1 }}
+                      className='flex-1'
+                      placeholder='输入元标签以搜索文章'
+                    >
+                      {SearchTags.map(tag => (
+                        <Mentions.Option key={tag} value={tag}>
+                          {tag}
+                        </Mentions.Option>
+                      ))}
+                    </Mentions>
+                    <Button type='primary' icon={<SearchOutlined />} htmlType='submit'></Button>
+                  </div>
+                </Form.Item>
+              </Form>
+            }
+            itemLayout='vertical'
+            size='large'
+            pagination={{
+              pageSize: 5,
+            }}
+            dataSource={files}
+            footer={
+              <div>
+                还没仔细研究过，不过本站的所有文章应该会使用 <b>CC-BY-SA-4.0</b> 协议
+              </div>
+            }
+            renderItem={item => <Post {...item} />}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
