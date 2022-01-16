558e2d96182bc7c776bc4091fb6f7d97761db299
kaixa <690750353@qq.com>
2022-01-16 22:41:19 +0800

    fix(Code): use cjs styles

--- components/markdown/Code.tsx
+++ components/markdown/Code.tsx
@@ -1,13 +1,27 @@
 import { ReactNode } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { solarizedLight, github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { CopyOutlined } from '@ant-design/icons';
+import { Button, message } from 'antd';
+import { github } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
 export const Code = (props: { className: string; children: ReactNode }) => {
   console.log(props);
   const language = props.className.replace('lang-', '');
-
   return (
-    <SyntaxHighlighter language={language} style={github}>
-      {props.children}
-    </SyntaxHighlighter>
+    <div className='flex flex-col'>
+      <div>
+        <Button
+          icon={<CopyOutlined />}
+          onClick={e => {
+            navigator.clipboard.writeText(props.children as string).then(() => {
+              message.info('代码已复制');
+            });
+          }}
+        />
+      </div>
+      <SyntaxHighlighter language={language} style={github}>
+        {props.children}
+      </SyntaxHighlighter>
+    </div>
   );
 };
