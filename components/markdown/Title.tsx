27c93cd111b5cf19a1f8a066993a696512f58049
kaixa <690750353@qq.com>
2022-01-18 20:16:51 +0800

    fix(title): error on blog/[id] page

--- components/markdown/Title.tsx
+++ components/markdown/Title.tsx
@@ -7,8 +7,12 @@ const { Title: AntdTitle } = Typography;
 export function Title(props: { children: [string] | [ReactNode]; level: 1 | 2 | 3 | 4 | 5 }) {
   const router = useRouter();
   return (
-    <AntdTitle level={props.level} className='ref-title'>
-      <Link href={router.pathname + '#' + props.children[0]}>
+    <AntdTitle
+      level={props.level}
+      className='ref-title'
+      id={typeof props.children[0] === 'string' ? props.children[0] : ''}
+    >
+      <Link href={router.asPath.slice(0, router.asPath.indexOf('#')) + '#' + props.children[0]}>
         {/* assume children is string */}
         {props.children[0]}
       </Link>
