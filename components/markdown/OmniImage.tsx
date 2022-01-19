7a9810d6827132f580d558150f5eca32d4dc0ff1
kaixa <690750353@qq.com>
2022-01-19 22:27:54 +0800

    feat(component): image style

--- components/markdown/OmniImage.tsx
+++ components/markdown/OmniImage.tsx
@@ -1,10 +1,11 @@
 import { SiteConfig } from '@/config/site';
+import Image from 'next/image';
 
 export function OmniImage(props: { alt: string; src: string; className: string }) {
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      className={props.className + ' max-w-full'}
+      className={props.className + ' max-w-full max-h-full object-contain object-center'}
       src={props.src.startsWith('@') ? SiteConfig.imageBucket + props.src.substring(1) : props.src}
       alt={props.alt}
     />
